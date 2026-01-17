cc60f64fab570cdbda90867f6c9016e98f5d302c
dialgi <111202214463@mhs.dinus.ac.id>
2026-01-17 15:17:18 +0700

    eror sidebar.blade.php

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware('admin')->prefix('admin')->name('admin.')->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+        // Category Management
+        Route::resource('categories', CategoryController::class);
+
+        // Event Management
+        Route::resource('events', EventController::class);
+    });
+});
+
+require __DIR__.'/auth.php';
